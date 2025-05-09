a6e4beec4115d37e5941d7ad6729654ede4faca1
Alpha-2310 <arshathda00@gmail.com>
2025-05-09 08:35:44 +0530

    user-login added

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -8,11 +8,10 @@ const AppContextProvider = (props) => {
 
     const currencySymbol = '$'
     const backendUrl = import.meta.env.VITE_BACKEND_URL
-    const [doctors, setDoctors] = useState([])
 
-    const value = {
-        doctors, currencySymbol
-    }
+    const [doctors, setDoctors] = useState([])
+    const [token, setToken] = useState(localStorage.getItem('token')?localStorage.getItem('token'):false)
+
 
     const getDoctorsData = async () => {
         try {
@@ -29,6 +28,11 @@ const AppContextProvider = (props) => {
             toast.error(error)
         }
     }
+
+    const value = {
+        doctors, currencySymbol, token, setToken, backendUrl
+    }
+
 
     useEffect(()=>{
         getDoctorsData()
