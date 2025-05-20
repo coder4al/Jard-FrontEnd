385696891b529dcdc5b32f105dfd06a1873e492c
Alpha-2310 <arshathda00@gmail.com>
2025-05-21 00:58:36 +0530

    Project-completed

--- src/pages/MyAppointments.jsx
+++ src/pages/MyAppointments.jsx
@@ -78,9 +78,11 @@ const MyAppointments = () => {
               <div></div>
 
               <div className='flex flex-col gap-2 justify-end'> 
-                {!item.cancelled && <button className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border border-gray-300 rounded hover:bg-secondary hover:text-white transition-all duration-300 cursor-pointer'>Pay Online</button>}
-                {!item.cancelled && <button onClick={()=>cancelAppointment(item._id)} className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border border-gray-300 rounded hover:bg-primary hover:text-white transition-all duration-300 cursor-pointer'>Cancel appointment</button>}
-                {item.cancelled && <button className='sm:min-w-48 py-2 border border-red-500 rounded text-red-500 '>Appointment Cancelled</button>}
+                {!item.cancelled && item.payment && !item.isCompleted && <button className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border border-gray-300 rounded hover:bg-secondary hover:text-white transition-all duration-300 cursor-pointer'>Paid</button>}
+                {!item.cancelled && !item.payment && !item.isCompleted && <button className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border border-gray-300 rounded hover:bg-secondary hover:text-white transition-all duration-300 cursor-pointer'>Pay Online</button>}
+                {!item.cancelled && !item.isCompleted && <button onClick={()=>cancelAppointment(item._id)} className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border border-gray-300 rounded hover:bg-primary hover:text-white transition-all duration-300 cursor-pointer'>Cancel appointment</button>}
+                {item.cancelled && !item.isCompleted && <button className='sm:min-w-48 py-2 border border-red-500 rounded text-red-500 '>Appointment Cancelled</button>}
+                {item.isCompleted && <button className='sm:min-w-48 py-2 border border-green-500 rounded text-green-600'>Completed</button>}
               </div>
             </div>
           ))}
--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -50,12 +50,12 @@ const MyProfile = () => {
           isEdit
           ? <label htmlFor="image">
             <div className="inline-block relative cursor-pointer">
-              <img className="w-36 rounded opacity-75" src={image ? URL.createObjectURL(image) : userData.image} alt="" />
+              <img className="w-40 rounded-full border border-gray-400 opacity-75" src={image ? URL.createObjectURL(image) : userData.image} alt="" />
               <img className="w-10 absolute bottom-12 right-12" src={image ? '' : assets.upload_icon} alt="" />
             </div>
             <input onChange={(e) => setImage(e.target.files[0])} type="file" id="image" hidden/>
           </label>
-          : <img className="w-36 rounded" src={userData.image} alt="" />
+          : <img className="w-40 rounded-full border border-gray-400" src={userData.image} alt="" />
         }
 
         {isEdit ? (
