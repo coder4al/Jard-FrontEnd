b604b893806adb5aad277972e0f85c49d94c6cc4
Alpha-2310 <arshathda00@gmail.com>
2025-05-21 20:23:49 +0530

    project-done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,13 +6,17 @@ import { AppContext } from "../context/AppContext";
 const Navbar = () => {
   const navigate = useNavigate();
 
-  const {token, setToken, userData} = useContext(AppContext)
+  const { token, setToken, userData } = useContext(AppContext)
 
   const location = useLocation()
   const isLogin = location.pathname === '/login'
 
 
   const [showMenu, setShowMenu] = useState(false);
+
+  const handleAdminRedirect = () => {
+    window.location.href = "http://localhost:5174"; // Redirect to admin panel
+  };
 
   const logout = () => {
     setToken(false)
@@ -21,7 +25,7 @@ const Navbar = () => {
 
   return (
     <div className="flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400">
-      <img onClick={()=>navigate('/')} className="w-41 cursor-pointer" src={assets.logo} alt="" />
+      <img onClick={() => navigate('/')} className="w-41 cursor-pointer" src={assets.logo} alt="" />
       <ul className="hidden md:flex items-start gap-5 font-medium">
         <NavLink to="/">
           <li className="py-1">HOME</li>
@@ -41,14 +45,20 @@ const Navbar = () => {
         </NavLink>
       </ul>
       <div className="flex items-center gap-4">
-        { token && !isLogin && userData ? (
+        <button
+          onClick={handleAdminRedirect}
+          className="bg-secondary/80 text-white px-8 py-3 rounded-full font-light hidden md:block cursor-pointer"
+        >
+          Admin Panel
+        </button>
+        {token && !isLogin && userData ? (
           <div className="flex items-center gap-2 cursor-pointer group relative">
             <img className="w-8 rounded-full" src={userData.image} alt="" />
             <img className="w-2.5" src={assets.dropdown_icon} alt="" />
             <div className="absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block">
               <div className="min-w-48 bg-stone-100 flex flex-col gap-4 p-4">
-                <p onClick={()=>navigate("/my-profile")} className="hover:text-black cursor-pointer">My Profile</p>
-                <p onClick={()=>navigate("/my-appointments")} className="hover:text-black cursor-pointer">My Appointments</p>
+                <p onClick={() => navigate("/my-profile")} className="hover:text-black cursor-pointer">My Profile</p>
+                <p onClick={() => navigate("/my-appointments")} className="hover:text-black cursor-pointer">My Appointments</p>
                 <p onClick={logout} className="hover:text-black cursor-pointer">Logout</p>
               </div>
             </div>
@@ -61,19 +71,26 @@ const Navbar = () => {
             Create Account
           </button>
         )}
-        <img onClick={()=>setShowMenu(true)} className="w-6 md:hidden" src={assets.menu_icon} alt="" />
+        <img onClick={() => setShowMenu(true)} className="w-6 md:hidden" src={assets.menu_icon} alt="" />
         {/* -------- Mobile view--------- */}
         <div className={` ${showMenu ? 'fixed w-full' : 'h-0 w-0'} md:hidden right-0 top-0 bottom-0 z-20 overflow-hidden bg-white transition-all`}>
           <div className="flex items-center justify-between px-5 py-6">
             <img className="w-36" src={assets.logo} alt="" />
-            <img className="w-7" onClick={()=>setShowMenu(false)} src={assets.cross_icon} alt="" />
+            <img className="w-7" onClick={() => setShowMenu(false)} src={assets.cross_icon} alt="" />
           </div>
+
           <ul className="flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium">
-            <NavLink onClick={()=>setShowMenu(false)} to='/'><p className='px-4 py-2 rounded inline-block'>HOME</p></NavLink>
-            <NavLink onClick={()=>setShowMenu(false)} to='/doctors'><p className='px-4 py-2 rounded inline-block'>ALL DOCTORS</p></NavLink>
-            <NavLink onClick={()=>setShowMenu(false)} to='/about'><p className='px-4 py-2 rounded inline-block'>ABOUT</p></NavLink>
-            <NavLink onClick={()=>setShowMenu(false)} to='/contact'><p className='px-4 py-2 rounded inline-block'>CONTACT</p></NavLink>
+            <NavLink onClick={() => setShowMenu(false)} to='/'><p className='px-4 py-2 rounded inline-block'>HOME</p></NavLink>
+            <NavLink onClick={() => setShowMenu(false)} to='/doctors'><p className='px-4 py-2 rounded inline-block'>ALL DOCTORS</p></NavLink>
+            <NavLink onClick={() => setShowMenu(false)} to='/about'><p className='px-4 py-2 rounded inline-block'>ABOUT</p></NavLink>
+            <NavLink onClick={() => setShowMenu(false)} to='/contact'><p className='px-4 py-2 rounded inline-block'>CONTACT</p></NavLink>
           </ul>
+          <div className="flex items-center gap-5 justify-center">
+
+            <NavLink onClick={() => setShowMenu(false)} to='/login'><p className='bg-primary text-white px-6 py-3 mt-10 rounded-full font-light md:block cursor-pointer'>Create Account</p></NavLink>
+            <NavLink onClick={handleAdminRedirect}><p className='bg-secondary text-white px-6 py-3 mt-10 rounded-full font-light md:block cursor-pointer'>Admin Panel</p></NavLink>
+
+          </div>
         </div>
       </div>
     </div>
